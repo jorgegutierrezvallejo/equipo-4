81b95a3121ebdbb8bde59e59a5a7d9d365294f9e
SneyderRodriguez <sneyder2112_@hotmail.com>
2026-06-30 11:38:16 -0500

    agrego ramaSneyder.js

---
+++ GitMerge/MergeIndividual.js
@@ -0,0 +1,10 @@
+let nombre = "Saul";
+console.log("Tu nombre es: " + nombre);
+let edad = "13";
+console.log("Tu edad es: " + edad);
+let ciudad = "Cartagena";
+console.log("Tu nombre es: " + nombre + " Tu edad es: " + edad + " Vives en: " + ciudad);
+
+let cliente = "Ana";
+let valor = 3000;
+console.log("Cliente:", cliente, "- Domicilio: $", valor);
---
+++ GitMerge/ramaSneyder.js
@@ -0,0 +1,7 @@
+let precio = 80.568;
+const IVA = 0.1;
+let descuento = IVA*precio;
+let total = precio-descuento;
+
+console.log("El descuento es del: " + descuento);
+console.log("El total es del: " + total);
